bc3a7435655ea1513c3922ca885f52dac9533358
Captain Nomis Universe <ch1st.hrmz@gmail.com>
2026-02-21 22:18:35 +0800

    [2] Flask Programming_knowledge
    Completed flask programming knowledge part two of video, will finish whole thing tomorrow

---
+++ pushups_logger/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+#This file initializes my Flask application
+
+#creates web application
+def create_app():
+    app = Flask(__name__)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)  
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    return app
---
+++ pushups_logger/main.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+
+main = Blueprint('main', __name__)  #create blueprint called main
+
+@main.route('/') #route for homepage (root)
+
+def index():
+    return render_template('index.html')  #when someone visits root, show this
+
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
